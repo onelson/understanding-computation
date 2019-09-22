06970e6607e61aab1388dea14d40ed149a816b72
Owen Nelson <onelson@gmail.com>
2019-09-21 19:52:35 -0700

    impl From<T> for Expr - hide the Rc::new(Box::new(x))) where possible.

--- examples/ch01-meaning.rs
+++ examples/ch01-meaning.rs
@@ -7,7 +7,7 @@ trait Expression {
         format!("«{}»", self.to_s())
     }
     fn to_s(&self) -> String;
-    fn as_value(&self) -> Option<&Value> {
+    fn as_number(&self) -> Option<&Number> {
         None
     }
 }
@@ -15,11 +15,9 @@ trait Expression {
 type Expr = Rc<Box<dyn Expression>>;
 
 #[derive(Clone)]
-enum Value {
-    Number(i64),
-}
+struct Number(i64);
 
-impl Expression for Value {
+impl Expression for Number {
     fn is_reducible(&self) -> bool {
         false
     }
@@ -30,12 +28,18 @@ impl Expression for Value {
 
     fn to_s(&self) -> String {
         match &self {
-            Value::Number(value) => format!("{}", value),
+            Number(value) => format!("{}", value),
         }
     }
 
-    fn as_value(&self) -> Option<&Value> {
+    fn as_number(&self) -> Option<&Number> {
         Some(&self)
+    }
+}
+
+impl From<Number> for Expr {
+    fn from(number: Number) -> Self {
+        Rc::new(Box::new(number))
     }
 }
 
@@ -48,18 +52,10 @@ impl Expression for Add {
 
     fn reduce(&self) -> Option<Expr> {
         match (self.0.is_reducible(), self.1.is_reducible()) {
-            (true, _) => Some(Rc::new(Box::new(Add(
-                self.0.reduce().unwrap(),
-                self.1.clone(),
-            )))),
-            (_, true) => Some(Rc::new(Box::new(Add(
-                self.0.clone(),
-                self.1.reduce().unwrap(),
-            )))),
-            _ => match (self.0.as_value(), self.1.as_value()) {
-                (Some(Value::Number(a)), Some(Value::Number(b))) => {
-                    Some(Rc::new(Box::new(Value::Number(a + b))))
-                }
+            (true, _) => Some(Add(self.0.reduce().unwrap(), self.1.clone()).into()),
+            (_, true) => Some(Add(self.0.clone(), self.1.reduce().unwrap()).into()),
+            _ => match (self.0.as_number(), self.1.as_number()) {
+                (Some(Number(a)), Some(Number(b))) => Some(Number(a + b).into()),
                 _ => panic!("Unexpected values"),
             },
         }
@@ -67,6 +63,12 @@ impl Expression for Add {
 
     fn to_s(&self) -> String {
         format!("{} + {}", self.0.to_s(), self.1.to_s())
+    }
+}
+
+impl From<Add> for Expr {
+    fn from(add: Add) -> Self {
+        Rc::new(Box::new(add))
     }
 }
 
@@ -79,18 +81,10 @@ impl Expression for Multiply {
 
     fn reduce(&self) -> Option<Expr> {
         match (self.0.is_reducible(), self.1.is_reducible()) {
-            (true, _) => Some(Rc::new(Box::new(Multiply(
-                self.0.reduce().unwrap(),
-                self.1.clone(),
-            )))),
-            (_, true) => Some(Rc::new(Box::new(Multiply(
-                self.0.clone(),
-                self.1.reduce().unwrap(),
-            )))),
-            _ => match (self.0.as_value(), self.1.as_value()) {
-                (Some(Value::Number(a)), Some(Value::Number(b))) => {
-                    Some(Rc::new(Box::new(Value::Number(a * b))))
-                }
+            (true, _) => Some(Multiply(self.0.reduce().unwrap(), self.1.clone()).into()),
+            (_, true) => Some(Multiply(self.0.clone(), self.1.reduce().unwrap()).into()),
+            _ => match (self.0.as_number(), self.1.as_number()) {
+                (Some(Number(a)), Some(Number(b))) => Some(Number(a * b).into()),
                 _ => panic!("Unexpected values"),
             },
         }
@@ -100,14 +94,18 @@ impl Expression for Multiply {
     }
 }
 
+impl From<Multiply> for Expr {
+    fn from(multiply: Multiply) -> Self {
+        Rc::new(Box::new(multiply))
+    }
+}
+
 fn main() {
-    let mut expr: Expr = Rc::new(Box::new(Multiply(
-        Rc::new(Box::new(Add(
-            Rc::new(Box::new(Value::Number(3))),
-            Rc::new(Box::new(Value::Number(5))),
-        ))),
-        Rc::new(Box::new(Value::Number(2))),
-    )));
+    let mut expr: Expr = Multiply(
+        Add(Number(3).into(), Number(5).into()).into(),
+        Number(2).into(),
+    )
+    .into();
 
     println!("{}", &expr.inspect());
 
